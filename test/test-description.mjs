ef2b08c4e3b99a2b859bab0d632391719546f9a2
RaimonMejias <alu0101390161@ull.edu.s>
2023-03-01 19:02:44 +0000

    Creación de los tests del programa

--- test/test-description.mjs
+++ test/test-description.mjs
@@ -1,20 +1,20 @@
 export default [
   {
-    input: 'test1.js',
-    output: 'logged1.js',
-    correctLogged: 'correct-logged1.js',
-    correctOut: 'logged-out1.txt'
+    input: 'input/test1.js',
+    output: 'js/logged1.js',
+    correctLogged: 'correct/correct-logged1.js',
+    correctOut: 'output/logged-out1.txt'
   },
   {
-    input: 'test2.js',
-    output: 'logged2.js',
-    correctLogged: 'correct-logged2.js',
-    correctOut: 'logged-out2.txt'
+    input: 'input/test2.js',
+    output: 'js/logged2.js',
+    correctLogged: 'correct/correct-logged2.js',
+    correctOut: 'output/logged-out2.txt'
   },
   {
-    input: 'test3.js',
-    output: 'logged3.js',
-    correctLogged: 'correct-logged3.js',
-    correctOut: 'logged-out3.txt'
+    input: 'input/test3.js',
+    output: 'js/logged3.js',
+    correctLogged: 'correct/correct-logged3.js',
+    correctOut: 'output/logged-out3.txt'
   },
 ]
--- test/test.mjs
+++ test/test.mjs
@@ -15,12 +15,40 @@ const Test = Tst.map(t => ({
 })
 )
 
+async function getLog(inputFile, outputFile, expectedFile) {
+  await transpile(inputFile, outputFile);
+  const output = await fs.readFile(outputFile, 'utf-8');
+  const expected = await fs.readFile(expectedFile, 'utf-8');
+  await fs.unlink(outputFile);
+  return [output, expected];  
+}
+
+async function calculate(outputLog, correctFile) {
+  const correct = await fs.readFile(correctFile, 'utf-8');
+  let log = console.log;
+  let result = ' ';
+  console.log = function (...space) { result += space.join(''); }
+  eval(outputLog);
+  console.log = log;
+  return [result, correct];
+}
+
 function removeSpaces(s) {
   return s.replace(/\s/g, '');
 }
 
 for (let i = 0; i < Test.length; i++) {
-  // Write your tests here   
+  describe(`transpile ${Test[i].input}`, async () => {
+    it(`tested with ${Test[i].correctLogged}`, async () => {
+      const result = getLog(Test[i].input, Test[i].output, Test[i].expected); 
+      assert.equal(removeSpaces(result[0]), removeSpaces(result[1]));
+    });
+    it(`tested with ${Test[i].correctOut}`, async () => {
+      const output =  getLog(Test[i].input, Test[i].output, Test[i].expected)[0]; 
+      const result = calculate(output, Test[i].correctOut)
+      assert.equal(removeSpaces(result[0]), removeSpaces(result[1]));
+    });
+  });
 }
 
 
